321733b564906fa9b76c91ed1e387ce40c6142ba
Rocío Marchetti <marchettirociob@gmail.com>
2023-04-25 17:24:30 +0200

    getBeerById service + beer-page.component method

--- src/app/beers/pages/beer-page/beer-page.component.ts
+++ src/app/beers/pages/beer-page/beer-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BeersService } from '../../services/beers.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Beer } from '../../interfaces/beer.interface';
 
 @Component({
   selector: 'app-beer-page',
   templateUrl: './beer-page.component.html',
-  styleUrls: ['./beer-page.component.css']
+  styleUrls: ['./beer-page.component.css'],
 })
-export class BeerPageComponent {
+export class BeerPageComponent implements OnInit {
 
+  public beer?: Beer;
+
+  constructor(
+    private beerService: BeersService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.beerService.getBeerById(id)))
+      .subscribe( beer => {
+        if(!beer) return this.router.navigate(['/beers/list']);
+        this.beer = beer;
+        console.log(beer)
+        return
+      });
+  }
 }
--- src/app/beers/services/beers.service.ts
+++ src/app/beers/services/beers.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Beer } from '../interfaces/beer.interface';
 
 @Injectable({
@@ -10,9 +10,16 @@ export class BeersService {
 
   constructor(private http: HttpClient) { }
 
-  API_URL: string = 'https://api.punkapi.com/v2/beers?page=1&per_page=12'
+  API_URL: string = 'https://api.punkapi.com/v2/beers'
 
   getBeerList(): Observable<Beer[]> {
     return this.http.get<Beer[]>(this.API_URL);
+  }
+
+  getBeerById( id: string ): Observable<Beer|undefined> {
+    return this.http.get<Beer>(`${this.API_URL}/${id}`)
+    .pipe(
+      catchError( error => of(undefined))
+    )
   }
 }
